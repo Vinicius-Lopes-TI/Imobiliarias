fcf45a8973376fde9ca62bb7409b84b8fa29d490
Vinícius Luiz Lopes <viniciuslopesti@hotmail.com>
2017-01-19 17:06:11 -0200

    Primeiro
    Criação do projeto

---
+++ Imobiliarias/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Imobiliarias.Startup))]
+namespace Imobiliarias
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
